96720232ea84300c8bf31d248f1f848a5ff7aa68
sridharaprasadhosahalli <sridharaprasadhosahalli@gmail.com>
2025-01-14 18:11:11 +0530

    test cases with httptest

--- main_test.go
+++ main_test.go
@@ -5,39 +5,12 @@ import(
     "net/http/httptest"
     "testing"
 	"encoding/json"
+	"bytes"
 )
-
-
-/*func TestGetItemsHandler(t *testing.T) {
-
-    req, _ := http.NewRequest("GET", "/items", nil)
-    w := httptest.NewRecorder()
-    r.ServeHTTP(w, req)
-
-    var items []item
-    json.Unmarshal(w.Body.Bytes(), &items)
-
-}*/
-
-/*func TestGetItemsHandler(t *testing.T){
-    res, err := http.Get("http://localhost:8080/items")
-    if err != nil {
-		log.Fatal(err)
-	}
-    body, err := io.ReadAll(res.Body)
-	res.Body.Close()
-	if res.StatusCode > 299 {
-		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
-	}
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s", body)
-}*/
 
 func TestGetItemsHandler(t *testing.T){
 
-	req := httptest.NewRequest(http.MethodGet, "/items", nil)
+	req := httptest.NewRequest("GET", "/items", nil)
     w := httptest.NewRecorder()
 	getorpostItems(w,req)
     var items []item
@@ -45,7 +18,58 @@ func TestGetItemsHandler(t *testing.T){
 	if(w.Code != http.StatusOK || len(items) < 3){
 		t.Error("Test Failed ", w.Code)
 	}
+}
 
+func TestGetItemByIdHandler(t *testing.T){
 
+	req := httptest.NewRequest("GET", "/items/1", nil)
+    w := httptest.NewRecorder()
+	getorputordeleteItems(w,req)
+    var item []item
+    json.Unmarshal(w.Body.Bytes(), &item)
+	if(w.Code != http.StatusOK || len(items) < 0){
+		t.Error("Test Failed ", w.Code)
+	}
+}
 
+func TestDeleteItemByIdHandler(t *testing.T){
+
+	req := httptest.NewRequest("DELETE", "/items/3", nil)
+    w := httptest.NewRecorder()
+	getorputordeleteItems(w,req)
+	if(w.Code != http.StatusOK || len(items) != 2){
+		t.Error("Test Failed ", w.Code)
+	}
+}
+
+func TestAddItemHandler(t *testing.T){
+	itemm := item{
+        ID: "4",
+        Name: "Orange",
+        Type: "Fruit",
+        Price: 60,
+    }
+    jsonValue, _ := json.Marshal(itemm)
+	req := httptest.NewRequest("POST", "/items", bytes.NewBuffer(jsonValue))
+    w := httptest.NewRecorder()
+	getorpostItems(w,req)
+	if(w.Code != http.StatusCreated || len(items) < 0){
+		t.Error("Test Failed ", w.Code)
+	}
+}
+
+func TestUpdateItemHandler(t *testing.T){
+	itemm := item{
+        ID: "1",
+        Name: "Orange",
+        Type: "Fruit",
+        Price: 60,
+    }
+    jsonValue, _ := json.Marshal(itemm)
+	req:= httptest.NewRequest("PUT", "/items/1", bytes.NewBuffer(jsonValue))
+    w := httptest.NewRecorder()
+	getorpostItems(w,req)
+	if(w.Code != http.StatusCreated || len(items) < 0 ){
+		t.Error("Test Failed ", w.Code)
+	}
 }
